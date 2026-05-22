d66ce33a928481f2807f793d67f14f1550ec931b
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-05-22 11:53:51 +0530

    Add initial script.groovy with function stubs

---
+++ script.groovy
@@ -0,0 +1,48 @@
+def codeCheckout() {
+
+}
+
+def mavenBuild() {
+
+}
+
+def npmBuild() {
+
+}
+
+def sonarAnalysis() {
+# A SonarQube analysis is the scanning process that examines your code for bugs and vulnerabilities
+}
+
+def testCoverage() {
+
+}
+
+def junitCoverage() {
+
+}
+
+
+def qualityGate() {
+# while a quality gate is the specific pass/fail criteria that determines if your code meets your release standards
+}
+
+def downloadNexusArtifact() {
+
+}
+
+def prepareDeploy() {
+
+}
+
+def deployApps() {
+
+}
+
+def downStream() {
+
+}
+
+def mailAlert() {
+
+}
