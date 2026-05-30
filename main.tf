f06accd6ed466ae6298d6484ffc42438f560fe29
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2026-05-30 21:57:50 +0530

    Remove project ID from Google Storage Bucket resource

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my_bucket" {
  name = "bkt-demo-000"
  location = "asia-south1"
- project = "project-b6ac39af-845d-4183-94e"
+ project = ""
  force_destory = true
  public_access_prevention = "enforced"
 }
